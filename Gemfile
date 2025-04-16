3980ef07846c5c19d6207d17c7ce0792235a3793
PatrickGpro <151019962+PatrickGpro@users.noreply.github.com>
2025-04-16 14:01:21 +0200

    function adding post and user

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "https://rubygems.org"
 
+gem 'image_processing', '~> 1.12'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 8.0.2"
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,10 @@
 class Photo < ApplicationRecord
+  belongs_to :user
   has_one_attached :image
-belongs_to :user
 
+  def display_image
+    return unless image.attached?
+
+    image.variant(resize_to_fill: [300, 600 , { gravity: 'Center' }]).processed
+  end
 end
